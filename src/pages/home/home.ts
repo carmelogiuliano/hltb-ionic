976fdd666516917dd3dd8b9423d0fcc42ccce6d8
Carmelo Giuliano <carmelo.giuliano1@gmail.com>
2017-01-28 00:32:05 +1100

    Add search field

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,30 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  query:string;
+  htmlResult:string;
+  data:any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.data = {};
+  }
+
+
+
+  search() {
+    var body = 'queryString=' + this.query;
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    this.http
+      .post('https://howlongtobeat.com/search_main.php?page=1',
+        body, {
+          headers: headers
+        })
+        .subscribe(data => {
+          this.htmlResult = data["_body"];
+        });
+
 
   }
 
