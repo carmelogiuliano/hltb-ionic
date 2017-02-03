bd54a80557d13e6763d53dee0f190ac5eaa99884
Carmelo Giuliano <carmelo.giuliano1@gmail.com>
2017-02-03 20:00:35 +1100

    Extract data from html

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,19 +8,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  htmlResult:string;
+  query:string;
 
   constructor(public navCtrl: NavController, public http: Http) {
 
   }
 
-
-  search(ev) {
-    var query = ev.target.value;
-    if(query === undefined) {
-      query = "";
-    }
-    var body = 'queryString=' + query;
+  search() {
+    var body = 'queryString=' + this.query;
     var headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
@@ -30,12 +25,40 @@ export class HomePage {
           headers: headers
         })
         .subscribe(data => {
-          this.htmlResult = data["_body"];
-          this.htmlResult = replaceAll(this.htmlResult, "gameimages/", "https://howlongtobeat.com/gameimages/");
+          var htmlStr = data["_body"];
+          htmlStr = replaceAll(htmlStr, "gameimages/", "https://howlongtobeat.com/gameimages/");
+
+
+          var el = document.createElement('html');
+          el.innerHTML = htmlStr;
+          var htmlData = el.getElementsByTagName('li');
+          var gameList = [];
+
+          for(var i = 0; i < htmlData.length; i++) {
+            var imgUrl = htmlData[i].getElementsByTagName('img')[0].src;
+            var title = htmlData[i].getElementsByTagName('a')[0].title;
+            var durations = htmlData[i].getElementsByClassName('center');
+            var durationData = htmlData[i].getElementsByClassName('search_list_details_block')[0].children;
+
+            //debugger;
+
+            var game = { durations: [] };
+            game['title'] = title;
+            game['imgUrl'] = imgUrl;
+            for(var j = 0; j < durationData.length; j++) {
+               var range = durationData[j].children[0].textContent;
+               var length = durationData[j].children[1].textContent;
+               game.durations.push({
+                  range: range,
+                  length: length
+               });
+            }
+
+            gameList.push(game);
+          }
+
+          debugger;
         });
-
-        
-
   }
 
 }
