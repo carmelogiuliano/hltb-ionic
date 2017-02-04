f673818cd20d2b297e43836ba12a62e4e3016ad2
Carmelo Giuliano <carmelo.giuliano1@gmail.com>
2017-02-04 12:09:38 +1100

    Shrink header on scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
+import { ShrinkHeader } from '../components/shrink-header/shrink-header';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     HomePage,
+    ShrinkHeader
   ],
   imports: [
     IonicModule.forRoot(MyApp)
